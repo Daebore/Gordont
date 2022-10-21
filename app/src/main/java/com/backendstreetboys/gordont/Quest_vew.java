f5497a08082d912cb100b1bd918a7b34c4bbbcea
AlonsoRial <alonsorivasalcobendas@gmail.com>
2022-10-21 13:12:43 +0200

    anyadir Quest_view

---
+++ app/src/main/java/com/backendstreetboys/gordont/Quest_vew.java
@@ -0,0 +1,13 @@
+package com.backendstreetboys.gordont;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Quest_vew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quest_vew);
+    }
+}
