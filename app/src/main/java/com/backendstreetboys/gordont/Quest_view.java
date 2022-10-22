41042a433e041114601726fbdafd553edf4edb50
AlonsoRial <alonsorivasalcobendas@gmail.com>
2022-10-22 22:18:57 +0200

    Anyadiendo imagenes chad al Quest v2

--- app/src/main/java/com/backendstreetboys/gordont/Quest_view.java
+++ app/src/main/java/com/backendstreetboys/gordont/Quest_view.java
@@ -7,13 +7,23 @@ import com.backendstreetboys.gordont.databinding.ActivityQuestViewBinding;
 
 public class Quest_view extends AppCompatActivity {
 
+    private ActivityQuestViewBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ActivityQuestViewBinding binding = ActivityQuestViewBinding.inflate(getLayoutInflater());
+        binding = ActivityQuestViewBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setContentView(R.layout.activity_quest_view);
     }
+
+    public void onRadioButtonClicked(Persona persona){
+
+
+
+    }
+
+
 }
