840a390d8612893cf299a0ba2ec2163402c13be7
Javier M <javier_m_h_1993@hotmail.com>
2022-10-25 23:16:01 +0200

    Rama Javier funcional de nuevo

--- app/src/main/java/com/backendstreetboys/gordont/MainActivity.java
+++ app/src/main/java/com/backendstreetboys/gordont/MainActivity.java
@@ -6,24 +6,49 @@ import android.os.Bundle;
 import com.backendstreetboys.gordont.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    /*
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            setContentView(R.layout.activity_main);
+            setContentView(binding.getRoot());
+
+        }
+
+       */
+    public static final String EL_IMC = "imc";
+    public static final String EL_MENSAJE = "mensaje";
+
 
     /*
-    * hola javi
-    * */
+     * hola javi
+     * */
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        Bundle extras = getIntent().getExtras();
+
+        double imc = extras.getDouble(EL_IMC);
+        String mensaje = extras.getString(EL_MENSAJE);
+
+        String texto_imc = String.valueOf(imc);
+
+        binding.IMCtext.setText(texto_imc);
+        binding.MENSAJEtext.setText(mensaje);
+
 
 
     }
 
 
 
-
-    }
-
-
+}
