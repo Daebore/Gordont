0ca547cc9f8bd893c2a812f98e1e322bbe5811dd
Samuel A <samaropulido@gmail.com>
2022-10-25 13:26:43 +0200

    solu error pantalla java main

--- app/src/main/java/com/backendstreetboys/gordont/MainActivity.java
+++ app/src/main/java/com/backendstreetboys/gordont/MainActivity.java
@@ -12,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
+        //setContentView(binding.getRoot());
 
     }
 
