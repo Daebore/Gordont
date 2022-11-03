ccd9f8026ea3994d00d2c537371444e161e8e98b
AlonsoRial <alonsorial@gmail.com>
2022-11-03 21:32:21 +0100

    arreglo de activity_dietas_ejercicios.xml

---
+++ app/src/main/java/com/backendstreetboys/gordont/DietasEjercicios.java
@@ -0,0 +1,13 @@
+package com.backendstreetboys.gordont;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DietasEjercicios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dietas_ejercicios);
+    }
+}
