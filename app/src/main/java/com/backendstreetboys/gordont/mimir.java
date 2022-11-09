911c3d8ffeb1302af0a18a8353a469c8b745a6c1
Daebore <javier_m_h_1993@hotmail.com>
2022-11-09 14:17:10 +0100

    yoda

---
+++ app/src/main/java/com/backendstreetboys/gordont/mimir.java
@@ -0,0 +1,14 @@
+package com.backendstreetboys.gordont;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mimir extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mimir);
+    }
+}
