819836b95f457495c087f52d87ae7dfd445331d4
amsamu <samaropulido@gmail.com>
2022-11-21 00:27:28 +0100

    Prevenir abrir otra pantalla mientras el cronómetro está corriendo.

--- app/src/main/java/com/backendstreetboys/gordont/MimirActivity.java
+++ app/src/main/java/com/backendstreetboys/gordont/MimirActivity.java
@@ -5,15 +5,19 @@ import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Chronometer;
+import android.widget.Toast;
+
 import com.backendstreetboys.gordont.database.AppDatabase;
 import com.backendstreetboys.gordont.database.HorasSueno;
 import com.backendstreetboys.gordont.databinding.ActivityMimirBinding;
+import com.backendstreetboys.gordont.util.AppToast;
+
 import java.time.Instant;
 
 public class MimirActivity extends AppCompatActivity {
 
     private ActivityMimirBinding binding;
-    boolean isChronometerRunning = false;
+    boolean chronometerRunning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,37 +35,53 @@ public class MimirActivity extends AppCompatActivity {
 
         // Botones de la barra de menú.
         binding.ButtonDietasEjercicios.setOnClickListener(v -> {
-            openDietasEjercicios();
+            if(chronometerRunning){
+                AppToast.showToast(this, "El cronómetro debe estar parado para ir a otra pantalla", Toast.LENGTH_SHORT);
+            }else{
+                openDietasEjercicios();
+            }
         });
         binding.ButtonBascula.setOnClickListener(v -> {
-            openComparar();
+            if(chronometerRunning){
+                AppToast.showToast(this, "El cronómetro debe estar parado para ir a otra pantalla", Toast.LENGTH_SHORT);
+            }else {
+                openComparar();
+            }
         });
         binding.ButtonHome.setOnClickListener(v -> {
-            openMain();
+            if(chronometerRunning){
+                AppToast.showToast(this, "El cronómetro debe estar parado para ir a otra pantalla", Toast.LENGTH_SHORT);
+            }else {
+                openMain();
+            }
         });
         binding.ButtonModificarPerfil.setOnClickListener(v -> {
-            openModificable();
+            if(chronometerRunning){
+                AppToast.showToast(this, "El cronómetro debe estar parado para ir a otra pantalla", Toast.LENGTH_SHORT);
+            }else {
+                openModificable();
+            }
         });
     }
 
     public void startCronometro() {
-        if(!isChronometerRunning){
+        if(!chronometerRunning){
             Chronometer simpleChronometer = (Chronometer) findViewById(R.id.simpleChronometer);
             simpleChronometer.setBase(SystemClock.elapsedRealtime());
             simpleChronometer.start();
-            isChronometerRunning = true;
+            chronometerRunning = true;
         }
     }
 
     public void stopCronometro() {
-        if(isChronometerRunning){
+        if(chronometerRunning){
             Chronometer simpleChronometer = (Chronometer) findViewById(R.id.simpleChronometer);
             // Tiempo contado por el cronómetro en milisegundos.
             long tiempo = SystemClock.elapsedRealtime() - simpleChronometer.getBase();
             simpleChronometer.stop();
             simpleChronometer.setBase(SystemClock.elapsedRealtime());
             anadirHorasSuenoBD(tiempo);
-            isChronometerRunning = false;
+            chronometerRunning = false;
         }
     }
 
